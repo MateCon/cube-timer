ede9d19c6227938c081d41549ec6093a8f25b0a5
MateCon <68882595+MateCon@users.noreply.github.com>
2021-08-02 19:45:35 -0300

    create useStopwatch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import useStopwatch from './helpers/useStopwatch';
 import './App.css';
 
 const App = () => {
+  const [minutes, seconds, hundreaths, start, pause, reset] = useStopwatch({ autoStart: false });
+
   return (
     <div className="App">
+      {`${minutes}:${seconds}.${hundreaths}`}
+      <button onClick={() => start()}>start</button> 
+      <button onClick={() => pause()}>pause</button>
+      <button onClick={() => reset()}>reset</button>
     </div>
   );
 }
