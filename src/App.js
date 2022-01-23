7f5b6b07dedc5c071e9d42d089052ec0642e3804
MateCon <68882595+MateCon@users.noreply.github.com>
2022-01-23 13:11:05 -0300

    remove console.log

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Stopwatch from './components/stopwatch/stopwatch';
-import ScrambleViewer from './components/scramble-viewer/scramble-viewer';
-import TimeViewer from './components/time-viewer/time-viewer';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Stopwatch from "./components/stopwatch/stopwatch";
+import ScrambleViewer from "./components/scramble-viewer/scramble-viewer";
+import TimeViewer from "./components/time-viewer/time-viewer";
+import "./App.css";
 
 const App = () => {
-  const [times, setTimes] = useState([]);
-  const [lastSolve, setLastSolve] = useState({});
+	const [times, setTimes] = useState([]);
+	const [lastSolve, setLastSolve] = useState({});
 
-  useEffect(() => {
-    console.table(times);
-    if(lastSolve.time === undefined) return;
-    setTimes([...times, lastSolve]);
-  }, [lastSolve]);
+	useEffect(() => {
+		if (lastSolve.time === undefined) return;
+		setTimes([...times, lastSolve]);
+	}, [lastSolve]);
 
-  return (
-    <div id="app">
-      <ScrambleViewer
-        lastSolve={lastSolve}
-      />
-      <TimeViewer 
-        times={times}
-      />
-      <Stopwatch
-        setLastSolve={setLastSolve}
-      />
-    </div>
-  );
-}
+	return (
+		<div id='app'>
+			<ScrambleViewer lastSolve={lastSolve} />
+			<TimeViewer times={times} />
+			<Stopwatch setLastSolve={setLastSolve} />
+		</div>
+	);
+};
 
 export default App;
